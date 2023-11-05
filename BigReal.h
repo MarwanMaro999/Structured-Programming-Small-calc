52940c6770e4df53ab4e023f24a2b09019bfd42b
abdo2o62 <110852803+abdo2o62@users.noreply.github.com>
2023-11-06 00:04:26 +0200

    final code 2

--- BigReal.h
+++ BigReal.h
@@ -2,34 +2,36 @@
 
 using namespace std;
 
-#ifndef ASS2_BIGREAL_H
-#define ASS2_BIGREAL_H
+#ifndef A2_OOP_BigReal_H
+#define A2_OOP_BigReal_H
 
 
 class BigReal {
 private:
-    char sign; // +,-
+    char sign; //+,-
     string integer, decimal;
     string fullRealNumber;
     //operations
     bool valid(string str);
+    void leading_zeros();
+
 public:
     //default constructor
     BigReal();
+    //constructor overloading
     BigReal(string str);
-
-    BigReal operator + (BigReal & another);
-
-//    BigReal operator - (BigReal & another);
-//
-//    bool operator == (BigReal & anotherReal);
-//
-//    bool operator > (BigReal & anotherReal);
-//
-//    bool operator < (BigReal & anotherReal);
-
+    //operator overloading
+    char get_sign();
+    int get_size();
+    BigReal& operator= (string str);
+    BigReal operator + (BigReal& another);
+    BigReal operator - (BigReal& another);
+    bool operator == (BigReal& anotherReal);
+    bool operator > (BigReal& anotherReal);
+    bool operator < (BigReal& anotherReal);
+    friend ostream& operator << (ostream& ccout,BigReal& BR);
     void print();
 
 };
 
-#endif //ASS2_BIGREAL_H
+#endif //A2_OOP_BigReal_H
